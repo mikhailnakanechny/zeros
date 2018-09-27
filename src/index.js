7155e2d6580f71eae9832b7614d00496c27f6268
mikhailnakanechny <mikhailnakanechny@gmail.com>
2018-09-28 00:23:48 +0300

    100 ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+  // https://tproger.ru/problems/how-many-zeros-at-the-end-of-the-factorial-of-100/
+    /*       Ноль в конце получается при умножении на 10
+Если расписать на простые множители, то при произведении 2 и 5
+Задача сводится к нахождению 5 в множителях, если каждое число в факториале
+расписать на простые множители.   */
+
+    let zCount = 0;
+    for (let i = 5; i <= number; i += 5) {
+        let y = i;
+        while( y % 5 === 0) {
+            y = y / 5;
+            zCount++;
+        }
+    }
+    return zCount;
+};
+
